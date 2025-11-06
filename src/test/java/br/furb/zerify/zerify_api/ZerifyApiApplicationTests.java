ea32c389ffe7a6a3a255d11aba27784d74a70133
wesley.zeitz <wesley.zeitz@senior.com.br>
2025-11-06 13:59:57 -0300

    Criação do projeto Spring

---
+++ src/test/java/br/furb/zerify/zerify_api/ZerifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.furb.zerify.zerify_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
